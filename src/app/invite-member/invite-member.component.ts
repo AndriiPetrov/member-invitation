e19469b5c5e21bdfdc91d1531d0fd8c46b1e550f
Andrii Petrov <apetrov@Andriis-iMac.local>
2018-11-02 18:54:59 +0200

    emprove forms

--- src/app/invite-member/invite-member.component.ts
+++ src/app/invite-member/invite-member.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MembersService } from '../shared/members.service';
+import { Member } from "../shared/member.model";
 
 @Component({
   selector: 'app-invite-member',
@@ -8,10 +9,33 @@ import { MembersService } from '../shared/members.service';
   styleUrls: ['./invite-member.component.css']
 })
 export class InviteMemberComponent implements OnInit {
+  members = this.service.data;
 
-  constructor(private currentRoute: ActivatedRoute, private sercive: MembersService) { }
+  constructor(private currentRoute: ActivatedRoute, public service: MembersService) { }
 
   ngOnInit() {
   }
 
+  addMember(firstName: HTMLInputElement,
+            lastName: HTMLInputElement,
+            eMail: HTMLInputElement,
+            noLeanCaseProject: HTMLInputElement,
+            dateStatusChange: HTMLInputElement,
+            dateLastLogin: HTMLInputElement,
+            Action: HTMLInputElement): boolean {
+    this.members.push( new Member(
+      firstName.value,
+      lastName.value,
+      eMail.value,
+      noLeanCaseProject.value,
+      dateStatusChange.value,
+      dateLastLogin.value,
+      Action.value
+      ));
+    firstName.value = '';
+    lastName.value = '';
+    eMail.value = '';
+
+      return false;
+  }
 }
